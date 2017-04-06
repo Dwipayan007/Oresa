00637ee1b9fece7e28375594aa4cf98cbeda6685
DwipayanT <daipayan.tripathy@idsnext.com>
2017-04-06 09:27:34 +0530

    Memberprofile added

---
+++ App/Controllers/memProfileViewCtrl.js
@@ -0,0 +1,15 @@
+﻿ores.controller("memProfileViewCtrl", ["$scope", "$http", "$location", "mobileCheck", "memProfileViewService", function ($scope, $http, $location, memProfileViewService,mobileCheck) {
+    if (mobileCheck) {
+        angular.element(document.querySelector("#about-tabs")).removeClass("all_pad");
+        angular.element(document.querySelector("#about-tabs")).addClass("all_pad1");
+    }
+    //$scope.fnGoToPage = function (args) {
+    //    $location.path('/' + args);
+    //}
+    $scope.memID = "";
+    $scope.GetMemberShipAllData = function () {
+        memProfileViewService.memProfileView($scope.memID).then(function (data) {
+
+        }
+    };
+}])
---
+++ App/Services/memProfileViewService.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../app.js" />
+ores.factory("memProfileViewService", ["$http", "$q", "baseService", function ($http, $q, baseService) {
+
+    var memProfileViewFactory = {};
+
+    var _memProfileView = function (memID) {
+        var deffer = $q.defer();
+        $http.get(baseService + 'api/memProfileView/', memID, {
+            headers: { 'Content-Type': 'application/json;charset=utf-8' }
+        }).success(function (data, status) {
+            deffer.resolve(data, status);
+        }).error(function (result, status) {
+            deffer.reject(result);
+        });
+        return deffer.promise;
+    };
+    memProfileViewFactory.memProfileView = __memProfileView;
+    return devSignupFactory;
+
+}]);
